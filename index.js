bc3e41ffe0d18592bc89789cde7a7487b8b184a3
Chris Bongers <chrisbongers@gmail.com>
2022-06-16 07:35:38 +0200

    feat: calling the Revue API

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ import fetch from 'node-fetch';
 
 dotenv.config();
 
-console.log(`I'm working!`)
+const callRevueAPI = async(endpoint) => {
+    const response = await fetch(`https://www.getrevue.co/api/v2/${endpoint}`, {
+        headers: {
+            Authorization: `Token ${process.env.REVUE_API_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        method: 'GET',
+    }).then((res) => res.json());
+    return response;
+}
+
+(async () => {
+    const revueUnsubscribed = await callRevueAPI('subscribers/unsubscribed');
+    console.log(revueUnsubscribed);
+
+    const revueSubscribed = await callRevueAPI('subscribers');
+    console.log(revueSubscribed);
+})();
