20d6b781f0931e7d8bad86662fe189143b09aa3f
Chris Bongers <chrisbongers@gmail.com>
2022-06-19 07:49:48 +0200

    fix: change port and host

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ fastify.post('/sendy-webhook', async function (request, reply) {
     throw new Error('Trigger not found')
 })
 
-fastify.listen({port: 3000}, function (err, address) {
+fastify.listen({port: process.env.PORT || 3000, host: '0.0.0.0'}, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
